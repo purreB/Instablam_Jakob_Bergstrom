c2574afeec3cb7e5f103f466fd75170be2826161
purreB <jakob.bergstrom@live.se>
2021-11-18 11:42:10 +0100

    Made a template, need to add a sw

---
+++ vite.config.js
@@ -0,0 +1,38 @@
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: ['favicon.svg', 'robots.txt'],
+      manifest: {
+        name: 'Instablam, by JakobB',
+        short_name: 'Instablam',
+        description:
+          'An application that lets you take pictures and save them to a gallery',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
+});
