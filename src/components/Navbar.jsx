f943dff48aee0b6a0c2e117ecdb555b7a1c26de6
purreB <jakob.bergstrom@live.se>
2021-11-23 13:06:03 +0100

    Added navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <>
+      <nav>
+        <a href=""></a>
+        <a href=""></a>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
