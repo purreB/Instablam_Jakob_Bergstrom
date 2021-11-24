3d4c142021ba57237742f2e7d7761d0600af875b
purreB <jakob.bergstrom@live.se>
2021-11-24 17:16:49 +0100

    Can now save imgs in localStorage together with date of capture, will soon add position aswell

--- src/components/Camera.jsx
+++ src/components/Camera.jsx
@@ -1,15 +1,15 @@
-import React, { useState, useCallback, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Webcam from 'react-webcam';
 
 function Camera() {
   const webcamRef = useRef(null);
   const [imgSrc, setImgSrc] = useState(null);
   const [md, setmd] = useState(false);
+  const [imgArr, setimgArr] = useState([]);
 
-  const capture = useCallback(() => {
-    const imageSrc = webcamRef.current.getScreenshot();
-    setImgSrc(imageSrc);
-  }, [webcamRef, setImgSrc]);
+  useEffect(() => {
+    console.log(imgArr);
+  }, [imgArr]);
 
   const handleCameraToggle = () => {
     if (md) {
@@ -19,16 +19,60 @@ function Camera() {
     }
   };
 
+  function addObjToArray(imgObj) {
+    setimgArr([...imgArr, imgObj]);
+  }
+
+  function capture() {
+    const imageSrc = webcamRef.current.getScreenshot({
+      width: 300,
+      height: 200,
+    });
+    setImgSrc(imageSrc);
+    saveToStorage(imageSrc);
+  }
+
+  async function saveToStorage(imgSrc) {
+    let current = new Date();
+    let cDate =
+      current.getFullYear() +
+      '-' +
+      (current.getMonth() + 1) +
+      '-' +
+      current.getDate();
+    let cTime =
+      current.getHours() +
+      ':' +
+      current.getMinutes() +
+      ':' +
+      current.getSeconds();
+    let dateTime = cDate + ' ' + cTime;
+    const imgObj = {
+      img: imgSrc,
+      position: '',
+      date: dateTime,
+    };
+    addObjToArray(imgObj);
+    const json = JSON.stringify(imgArr);
+    localStorage.setItem('Images', json);
+  }
+
   return (
     <>
-      {md ? (
-        <Webcam audio={false} ref={webcamRef} screenshotFormat={'image/jpeg'} />
-      ) : null}
-      <button onClick={handleCameraToggle}>
-        {md ? 'Turn Camera Off' : 'Turn Camera On'}
-      </button>
-      <button onClick={capture}>Capture Photo</button>
-      {imgSrc && <img src={imgSrc} />}
+      <section id="Camera">
+        {md ? (
+          <Webcam
+            audio={false}
+            ref={webcamRef}
+            screenshotFormat={'image/jpeg'}
+          />
+        ) : null}
+        <button onClick={handleCameraToggle}>
+          {md ? 'Turn Camera Off' : 'Turn Camera On'}
+        </button>
+        <button onClick={capture}>Capture Photo</button>
+        {imgSrc && <img src={imgSrc} />}
+      </section>
     </>
   );
 }
